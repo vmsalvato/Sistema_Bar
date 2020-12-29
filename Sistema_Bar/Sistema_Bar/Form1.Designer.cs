adaa2b68f7cefe324dfbd8c62c0a4b9b19b4b1ff
vmsalvato <viniciusmoreirasalvato@gmail.com>
2020-12-28 23:55:40 -0300

    commit up1

---
+++ Sistema_Bar/Sistema_Bar/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Sistema_Bar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(229, 84);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(238, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(143, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sistema Bar";
+            // 
+            // picImagem
+            // 
+            this.picImagem.Location = new System.Drawing.Point(31, 130);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(179, 153);
+            this.picImagem.TabIndex = 3;
+            this.picImagem.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "CÓDIGO DO PRODUTO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(146, 296);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "TOTAL:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 358);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.picImagem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Sistema_Bar/Sistema_Bar/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Bar
+{
+    public partial class Form1 : Form
+    {
+        string[] produtos = new string[10];
+        string[] codigo = new string[10];
+        double[] valor = new double[10];
+        double soma;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            /* if (textBox1.Text.Length == 5)
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Text = "";
+                textBox1.Focus();
+
+            } */
+            if (textBox1.Text.Length == 4)
+            {
+                int indice = 0;
+                for (int prod = 1; prod < codigo.Length; prod ++)
+                {
+                    if(textBox1.Text == codigo[prod])
+                    {
+                        indice = prod;
+                    }
+                }
+                if (indice == 0)
+                {
+                    MessageBox.Show("Produto não encontrado");
+                }
+                else
+                {
+                    listBox1.Items.Add(textBox1.Text + "--" + produtos[indice] + "-- R$ " + valor[indice]);
+                    soma = soma + valor[indice];
+                    label3.Text = ("Valor Total R$ " + soma);
+                    picImagem.ImageLocation = "../Sistema_Bar/image/" + codigo[indice] + ".png";
+                    textBox1.Text = "";
+                    textBox1.Focus();
+                }
+            }
+    
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadArray();
+            soma = 0;
+        }
+
+        private void loadArray()
+        {
+            codigo[1] = "0001";
+            codigo[2] = "0002";
+            codigo[3] = "0003";
+            codigo[4] = "0004";
+            codigo[5] = "0005";
+
+            produtos[1] = "Cerveja";
+            produtos[2] = "Lanche";
+            produtos[3] = "Chocolate";
+            produtos[4] = "Bala";
+            produtos[5] = "Suco";
+
+            valor[1] = 7.00;
+            valor[2] = 12.00;
+            valor[3] = 4.50;
+            valor[4] = 2.50;
+            valor[5] = 6.00;
+        }
+    }
+}
